06a1edaaa7bad40b1b4663676047965402f6e58c
Пестов Алексей Анатольевич (4058309) <aapestov@vtb.ru>
2023-11-03 12:34:19 +0300

    StartObject remove

--- src/main/java/org/jgroups/demo/rpc/main/App.java
+++ src/main/java/org/jgroups/demo/rpc/main/App.java
@@ -5,8 +5,6 @@ import org.jgroups.demo.rpc.Node;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 class App {
     static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
